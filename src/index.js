0151ed7615e971e5721d7462ce50ba4f342ceba3
AshVosem <ash.vosem@gmail.com>
2020-03-04 01:01:59 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if(str.length%2 !== 0) return false
+
+    let brackets
+
+    for(let i = 0; i < bracketsConfig.length; ) {
+        brackets = bracketsConfig[i].join('')
+        if(str.includes(brackets)) {
+            str = str.replace(brackets, '')
+            i = 0
+        }
+        else i++
+    } 
+   
+    return str.length === 0 ? true : false 
 }
